c46ce431943165b0b2e9885a5fe72d67490b8dc7
Dominik Jungowski <Dominik@Jungowski.de>
2016-10-13 14:25:33 +0200

    junit reporter

--- karma.conf.js
+++ karma.conf.js
@@ -34,7 +34,12 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'junit'],
+
+	junitReporter: {
+		outputDir: 'test-reports',
+		outputFile: 'ifx-js-test-xunit.xml'
+	},
 
 
     // web server port
